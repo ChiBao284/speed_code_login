a150a03678a78c1a974f78505468d989b5e52644
chibao284 <chibao284@gmail.com>
2022-11-23 22:20:42 +0700

    flutter basic

---
+++ lib/Screens/welcome_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:speed_code_login/Screens/components/body.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Body(),
+    );
+  }
+}
